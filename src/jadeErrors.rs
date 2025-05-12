d4bff34b85a424e43f71a0ae5a60db03fecb8698
Opulent0 <galacticnoob772@gmail.com>
2025-05-12 08:21:14 -0500

    Added some new errors to use

--- src/jadeErrors.rs
+++ src/jadeErrors.rs
@@ -1,7 +1,24 @@
+use crate::runtime::Value;
+/*
+
 pub fn unknownSyntaxError(line:&str) {
     panic!("Error! unkown line! {line} {}", "\n\nDyl, Remember to add more to this error so it actually gives useful info!!!");
 }
 
 pub fn noEndingSemiColon(line:&str) {
     panic!("Error! line needs semicolon! {line}");
+}
+
+*/
+
+pub fn parsingError() {
+    panic!("WTF is this dude!!? You never told me this was an option!?")
+}
+
+pub fn valueError(type_str: &str, other: Value) {
+    panic!("Error! Type mismatch! Expected: {type_str}, Found: {:?}", other);
+}
+
+pub fn variableNotFoundError(name: &str) {
+    eprintln!("Variable '{}' not found", name);
 }
